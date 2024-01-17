7cd87ec6d1144fc6a090b50d5bab727c1e65b7be
mityaiii <pericle04@mail.ru>
2024-01-17 21:57:20 +0300

    update main

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import asyncio
 import os
 
-from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
 from aiogram.enums.parse_mode import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
